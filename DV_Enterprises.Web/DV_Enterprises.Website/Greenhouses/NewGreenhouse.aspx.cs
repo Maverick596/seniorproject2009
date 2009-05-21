e1f28ea196df40a5f907e3068c2c276ecf4474db
sthoms28 <sthoms@comcast.net>
2009-05-20 20:27:47 -0400

    add code for new greenhouse

--- DV_Enterprises.Web/DV_Enterprises.Website/Greenhouses/NewGreenhouse.aspx.cs
+++ DV_Enterprises.Web/DV_Enterprises.Website/Greenhouses/NewGreenhouse.aspx.cs
@@ -10,6 +10,7 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using DV_Enterprises.Web.Data.DataAccess.SqlRepository;
 
 public partial class Greenhouses_NewGreenhouse : System.Web.UI.Page
 {
@@ -21,6 +22,28 @@ public partial class Greenhouses_NewGreenhouse : System.Web.UI.Page
     protected void btnSave_Click(object sender, EventArgs e)
     {
         // Capture form information and store in database
+        DataContext db = new DataContext();
+        Address newAddress = new Address();
+        newAddress.StreetLine1 = txtAddress1.Text;
+        newAddress.StreetLine2 = txtAddress2.Text;
+        newAddress.City = txtCity.Text;
+        newAddress.StateOrProvince = txtState.Text;
+        newAddress.Zip = Convert.ToInt32(txtZipCode.Text);
+        newAddress.Country = txtCountry.Text;
+        newAddress.DateCreated = DateTime.Now;
+
+        db.Addresses.InsertOnSubmit(newAddress);
+        
+        
+        db.SubmitChanges();
+
+        Greenhouse newGreenhouse = new Greenhouse();
+        newGreenhouse.AddressID = newAddress.AddressID;
+
+        db.Greenhouses.InsertOnSubmit(newGreenhouse);
+        db.SubmitChanges();
+
+        
 
         // Close current window and refresh Parent window
 
