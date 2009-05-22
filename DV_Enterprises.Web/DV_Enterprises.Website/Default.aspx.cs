35eeb71ccf76d1c5ecf5e61be6f3a7870650dc9e
Louis T <negonicrac@gmail.com>
2009-05-21 20:35:42 -0400

    did something to the Default page dky but I will probably remove it soon

--- DV_Enterprises.Web/DV_Enterprises.Website/Default.aspx.cs
+++ DV_Enterprises.Web/DV_Enterprises.Website/Default.aspx.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Web.UI;
+using DV_Enterprises.Web.Presenter.Root;
+using DV_Enterprises.Web.Presenter.Root.Interface;
 
-public partial class Default : Page
+public partial class Default : Page, IDefault
 {
+    private DefaultPresenter _presenter;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        _presenter = new DefaultPresenter();
+        _presenter.Init(this);
     }
+
 }
