598bc67b54508b41398930715a0c2bdb5e17501c
Louis T <negonicrac@gmail.com>
2009-05-22 17:08:33 -0400

    remove silly typos. problems still left.

--- DV_Enterprises.Web/DV_Enterprises.Website/Default.aspx.cs
+++ DV_Enterprises.Web/DV_Enterprises.Website/Default.aspx.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Web.UI;
-using DV_Enterprises.Web.Presenter.Root;
-using DV_Enterprises.Web.Presenter.Root.Interface;
+//using DV_Enterprises.Web.Presenter.Root;
+//using DV_Enterprises.Web.Presenter.Root.Interface;
 
-public partial class Default : Page, IDefault
+public partial class Default : Page//, IDefault
 {
-    private DefaultPresenter _presenter;
+    //private DefaultPresenter _presenter;
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        _presenter = new DefaultPresenter();
-        _presenter.Init(this);
+        //_presenter = new DefaultPresenter();
+        //_presenter.Init(this);
     }
 
 }
