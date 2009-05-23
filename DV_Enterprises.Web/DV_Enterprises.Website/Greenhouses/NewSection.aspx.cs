1d02f097fa93a18ee8edf4b4f5a0c07d97ba6b23
sthoms28 <sthoms@comcast.net>
2009-05-23 00:15:57 -0400

    added page for creating new sections

---
+++ DV_Enterprises.Web/DV_Enterprises.Website/Greenhouses/NewSection.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using DV_Enterprises.Web.Data.Domain;
+public partial class Greenhouses_NewSection : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+
+
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        var s = new Section
+        {
+          Name = txtSectionName.Text,
+          CropID = Convert.ToInt32(ddlCropName.SelectedValue),
+          IdealTemperture = Convert.ToInt32(ddlTemp.SelectedValue),
+          TempertureTreshold = Convert.ToInt32(ddlTemp.SelectedValue),
+          IdealLightIntensity = Convert.ToInt32(ddlLight.SelectedValue),
+          LightIntensityTreshold = Convert.ToInt32(ddlLightThreshold.SelectedValue),
+          IdealHumidity = Convert.ToInt32(ddlHumidity.SelectedValue),
+          HumidityTreshold = Convert.ToInt32(ddlHumidityThreshold.SelectedValue),
+         
+                    
+        };
+        s.Save();
+    }
+}
