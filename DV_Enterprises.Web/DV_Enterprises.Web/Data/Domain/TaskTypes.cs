12d57a851d16195de83c2975ffa1ca3f12bf14ee
Louis T <negonicrac@gmail.com>
2009-06-03 18:22:53 -0400

    updated the Tasks

--- DV_Enterprises.Web/DV_Enterprises.Web/Data/Domain/TaskTypes.cs
+++ DV_Enterprises.Web/DV_Enterprises.Web/Data/Domain/TaskTypes.cs
@@ -1,4 +1,23 @@
 namespace DV_Enterprises.Web.Data.Domain
 {
-    public enum TaskTypes { Temperature, LightIntensity, Humidity }
+    /// <summary>
+    /// Task override the automatic settings that come from the sensors and may be dangerous
+    /// </summary>
+    public enum TaskTypes
+    { 
+        // Humidity Control Module
+        Humidifying,
+        Dehumidifying,
+
+        // Water Level Control Module
+        Watering,
+        NoWatering,
+        
+        // Lighting Control Module
+        Lighting, OverrideLighting,
+        
+        // Temperature Contro Module
+        Heating,
+        Cooling
+    }
 }
